0b9357fefab8bae4f55245fb8cb0c732b37e6e7b
JimmyBonez <complexsion360@gmail.com>
2016-09-10 04:42:25 -0700

    asd
    adf

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.listen(3000, function(){
+  console.log('Example app listening on port 3000!');
+});
